f3729ac678a6dfad11b0aa01120e80519368c66f
JoseHurtado03 <jose.hurtado@correo.unimet.edu.ve>
2023-01-24 21:36:45 -0400

    :ambulance: Ejercicios EDD

---
+++ Listas/src/listas/Nodo.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Nodo {
+    private Object element;
+    private Nodo next;
+    
+    public Nodo(Object elemento){
+        this.element = elemento;
+        this.next = null;
+    }
+    public Object getElement() {
+        return element;
+    }
+
+    public void setElement(Object element) {
+        this.element = element;
+    }
+
+    public Nodo getNext() {
+        return next;
+    }
+
+    public void setNext(Nodo next) {
+        this.next = next;
+    }
+}
